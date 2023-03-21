ed83f896c1c0baad1245d151bac39216a1f947a5
Parselinho <parselinho@gmail.com>
2023-03-21 20:36:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const { projects } = require('./data.json');
+const path = require('path');
+
+const app = express();
+
+
+app.set('view engine, pug');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
