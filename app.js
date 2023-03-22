80ca97a2ef5eaf7a0d0fb21cc7a1dfaf5144e5d8
Parselinho <parselinho@gmail.com>
2023-03-22 11:23:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,3 +10,12 @@ app.set('view engine, pug');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+    res.render('index', { projects });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.listen();
