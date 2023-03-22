a8ffb89a9096153405228cf358ad12c0ca685810
Parselinho <parselinho@gmail.com>
2023-03-22 15:46:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const { projects } = require('./data.json');
 const path = require('path');
-console.log(projects);
 const app = express();
 
 
-app.set('view engine, pug');
-
+app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req, res) => {
     res.render('index', { projects });
@@ -18,4 +17,12 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.listen();
+app.get('/project/:id', (req, res) => {
+    const projectId = req.params.id;
+    const project = projects.find(proj => proj.id === projectId);
+    res.render('project', { project });
+});
+
+app.listen(3001, () => {
+    console.log('im working')
+});
