370d0e1bdad7b3436f963f84417e890bd7bd81c6
Parselinho <parselinho@gmail.com>
2023-03-22 19:29:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,23 +4,45 @@ const path = require('path');
 const app = express();
 
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
     res.render('index', { projects });
+    // res.json({ projects });
 });
 
-app.get('/about', (req, res) => {
+app.get('/about', (req, res, next) => {
     res.render('about');
 });
 
-app.get('/project/:id', (req, res) => {
+app.get('/project/:id', (req, res, next) => {
     const projectId = req.params.id;
-    const project = projects.find(proj => proj.id === projectId);
-    res.render('project', { project });
+    const projectSID = projects.map(proj => proj.id) // to get all the projects ID for the error message
+    const project = projects.find( ({ id }) => id === +projectId); // (( proj ) => proj.id === +projectId); -- the {id} solution can also wrriten like that
+    if(project) {
+        res.render('project', { project });
+    } else {
+        const err = new Error(`Project Id <strong>-${projectId}-</strong> Is Not Found try to choose One of <strong>${projectSID}</strong> Id's`);
+        err.status = 404;
+        return next(err);
+    }
+});
+
+app.use((req, res, next) => {
+    const err = new Error('Page Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    err.status = err.status || 500;
+    err.message = err.message || 'Server Error'
+    res.status(err.status);
+    res.send(`Error ${err.status}: ${err.message}. <br><br><br><a href="/">Back to Home Page</a>`)
 });
 
 app.listen(3001, () => {
